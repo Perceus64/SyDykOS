580de7d220b7409dad3c4e03999728cd54656bac
Charbel Bachaalany <cbachaalany@gmail.com>
2023-05-08 23:52:08 +0300

    Update kderice-restore.sh

--- scripts/kderice-restore.sh
+++ scripts/kderice-restore.sh
@@ -1,1 +1,8 @@
- 
+#!/bin/bash
+
+export PATH=$PATH:~/.local/bin
+cp -r $HOME/$SCRIPTHOME/configs/.config/* $HOME/.config/
+pip install konsave
+konsave -i $HOME/$SCRIPTHOME/configs/sydyk.knsv
+sleep 1
+konsave -a sydyk
