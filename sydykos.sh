75a1e180c6ffaea9f3df52e9853cdc02f84f3621
Charbel Bachaalany <cbachaalany@gmail.com>
2023-05-27 18:13:00 +0300

    Update sydykos.sh

--- sydykos.sh
+++ sydykos.sh
@@ -18,16 +18,16 @@ echo -ne "
 -------------------------------------------------------------------------
                     Automated Arch Linux Installer
 -------------------------------------------------------------------------
-                Scripts are in directory named SyDYkOS
+                Scripts are in directory named SyDykOS
 "
     ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
       source $CONFIGS_DIR/setup.conf
     ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
-    ( arch-chroot /mnt $HOME/SyDYkOS/scripts/1-setup.sh )|& tee 1-setup.log
+    ( arch-chroot /mnt $HOME/SyDykOS/scripts/1-setup.sh )|& tee 1-setup.log
     if [[ ! $DESKTOP_ENV == server ]]; then
-      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/SyDYkOS/scripts/2-user.sh )|& tee 2-user.log
+      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/SyDykOS/scripts/2-user.sh )|& tee 2-user.log
     fi
-    ( arch-chroot /mnt $HOME/SyDYkOS/scripts/3-post-setup.sh )|& tee 3-post-setup.log
+    ( arch-chroot /mnt $HOME/SyDykOS/scripts/3-post-setup.sh )|& tee 3-post-setup.log
     cp -v *.log /mnt/home/$USERNAME
 
 echo -ne "
